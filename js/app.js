ea0f0ec26dcbbe47ba44fcda7f23be21fe5135d3
Rafal Cholaj <raf.cholaj@gmail.com>
2017-04-06 18:41:25 +0200

    added next generation computing

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ document.addEventListener('DOMContentLoaded', function() {
     this.height = boardHeight;
     this.board = document.querySelector('#board');
     this.cells = [];
+    this.index = function(x, y) {
+      var position = x + y * this.width;
+      return this.cells[position];
+    };
     this.createBoard = function() {
 
       this.board.style.width = this.width * 10 + 'px';
@@ -27,9 +31,82 @@ document.addEventListener('DOMContentLoaded', function() {
 
     };
 
+    this.setCellState = function(x, y, state) {
+      this.index(x, y).classList.add(state);
+    };
+
+    this.computeCellNextState = function(x, y) {
+
+      var totalLiveNeighbours = 0;
+
+      // console.log(this.index(x - 1, y - 1).classList);
+      // console.log(this.index(x + 1, y + 1).classList);
+      if (this.index(x - 1, y - 1) !== undefined && this.index(x - 1, y - 1).classList.contains('live')) {
+        totalLiveNeighbours++;
+      }
+
+      if (this.index(x, y - 1) !== undefined && this.index(x, y - 1).classList.contains('live')) {
+        totalLiveNeighbours++;
+      }
+
+      if (this.index(x + 1, y - 1) !== undefined && this.index(x + 1, y - 1).classList.contains('live')) {
+        totalLiveNeighbours++;
+      }
+
+      if (this.index(x - 1, y) !== undefined && this.index(x - 1, y).classList.contains('live')) {
+        totalLiveNeighbours++;
+      }
+
+      if (this.index(x + 1, y) !== undefined && this.index(x + 1, y).classList.contains('live')) {
+        totalLiveNeighbours++;
+      }
+
+      if (this.index(x - 1, y + 1) !== undefined && this.index(x - 1, y + 1).classList.contains('live')) {
+        totalLiveNeighbours++;
+      }
+
+      if (this.index(x, y + 1) !== undefined && this.index(x, y + 1).classList.contains('live')) {
+        totalLiveNeighbours++;
+      }
+
+      if (this.index(x + 1, y + 1) !== undefined && this.index(x + 1, y + 1).classList.contains('live')) {
+        totalLiveNeighbours++;
+      }
+
+      if (totalLiveNeighbours === 2 || totalLiveNeighbours === 3) {
+        return 1;
+      } else {
+        return 0;
+      }
+
+    };
+
+    this.nextGeneration = [];
+
+    this.computeNextGeneration = function() {
+
+      for (var i = 0; i < this.width; i++) {
+        for (var j = 0; j < this.width; j++) {
+          this.nextGeneration.push(this.computeCellNextState(j, i));
+
+        }
+      }
+      console.log(this.nextGeneration);
+    };
+
+    this.firstGlider = function() {
+      this.setCellState(0, 0, 'live');
+      this.setCellState(1, 0, 'live');
+      this.setCellState(2, 0, 'live');
+      this.setCellState(3, 0, 'live');
+      this.setCellState(4, 0, 'live');
+    };
+
   }
 
   var game = new GameOfLife(10,10);
   game.createBoard();
+  game.firstGlider();
+  game.computeNextGeneration();
 
 });
